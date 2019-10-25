65e88a1d6100d2a7fa4f4da0daea0d49cb53becf
matva22 <sokol-matik2002xd@mail.ru>
2019-10-25 22:34:23 +0300

    ADSDSDSDASDEQWEWQEQE;X:x

---
+++ 3v11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3v11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerimitrPloshadPloshadOkr();
+            
+        }
+        
+        public static void PerimitrPloshadPloshadOkr()
+        {
+            
+            Console.WriteLine("Введите чему равна сторона a");
+            var x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите чему равна сторона б");
+            var v = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите чему равна сторона с");
+            var c = Convert.ToDouble(Console.ReadLine());
+            if ((x > 0) && (v > 0) && (c > 0) && (x + v > c) && (v + c > x) && (c + x > v))
+            { 
+                double per = x + c + v;                
+                double plo;
+                double p = ((v + c + x) / 2);
+                double ploshad1 = (p * (p - x) * (p - v) * (p - c));               
+                plo = Math.Sqrt(ploshad1);                
+                double radius = ((p - x) * (p - v) * (p - c) / p);
+                double PloshadTreugSOkrug =per/4*radius;
+
+
+
+
+
+                Console.WriteLine($"Перимитр равен"+ per);
+                Console.WriteLine($"Площадь равна" + plo);
+                Console.WriteLine($"Площадь треугольника, вписанного в окружность равна "+ PloshadTreugSOkrug);
+
+
+               
+            }
+            else
+            {
+                Console.WriteLine("Такого треугольника не существует");
+            }
+            Console.ReadKey();
+        }
+        
+                  
+            
+        }
+    }
+
