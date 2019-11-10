a8efb545ef82f5351df637696be4b91a0536f383
matva22 <56400847+matva22@users.noreply.github.com>
2019-11-10 17:17:47 +0300

    задание игоря

---
+++ klasswork/zadanieigorya/zadanieigorya/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanieigorya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сколько  должен содержать чисел массив");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите порядковый номер,который хотите удалить");
+            var a = Convert.ToInt32(Console.ReadLine(     ));
+            metod(x,a);
+            Console.ReadKey();
+
+        }
+        public static void metod(int x,int a)
+        {
+            int[] nums = new int[x];
+            Random rand = new Random();          
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(1, 60);
+                
+                if((a>i) | (a<i))
+                  {
+                    Console.WriteLine($"порядковй номер "+(i)+" равен "+nums[i]) ;
+                }                                                              
+            }
+            Console.WriteLine($"удалённый:↓↓↓↓↓↓");
+            Console.Write($"порядковй номер " + (a) + " равен " + nums[a]);
+
+        }
+    }
+}
